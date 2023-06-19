c92b08f0688e948385230a6bea32c8c058468c54
mbt1 <mbt1@users.noreply.github.com>
2023-06-19 12:45:21 -0400

    moved node install to nonroot.sh

---
+++ packer/http/late_nonroot.sh
@@ -0,0 +1,10 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+command -v nvm
+nvm --version
+nvm install --lts
+node -v
+npm -v
+npm install -g npm
+npm install -g express
